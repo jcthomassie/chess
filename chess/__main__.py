23fb8eaa0ff9968bbfa166af1552659c66bd98a9
Julian Thomassie <jthomass@caltech.edu>
2019-03-26 20:43:58 -0700

    Added commandline argument parser

--- chess/__main__.py
+++ chess/__main__.py
@@ -4,11 +4,19 @@ Created on Sun Mar 24 10:45:07 2019
 
 @author: jctho_000
 """
-
+import argparse
 from chess import core, gui
 
 def main():
-    board = core.Board("Standard")
+    # Parse commandline args
+    parser = argparse.ArgumentParser(description="Play a chess game.")
+
+    parser.add_argument("fen", nargs="?", default="Standard",
+                        help="Starting position FEN string")
+    args = parser.parse_args()
+
+    # Setup board and GUI
+    board = core.Board(args.fen)
     with gui.Game(board) as game:
         game.loop()
     return
